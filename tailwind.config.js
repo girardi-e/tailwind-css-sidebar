fb239c97911fe7f68a16630beae5de6c0fb8568d
emilio girardi <emilio.g.girardi@gmail.com>
2021-11-10 15:09:25 +0100

    react icons package

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202225",
+        secondary: "#5865f2",
+        gray: colors.trueGray
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
